c7466aec98741381f3c98b1df7a1276921867108
Mrunank Pawar <mrunankpawar05@gmail.com>
2022-04-16 12:04:34 +0530

    added data-work

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFilppedCard = false;
+let firstCard, secondCard;
+
 function flipcard(){
-    console.log('I was clicked');
-    console.log(this);
+    this.classList.toggle('flip');
+
+    if(!hasFilppedCard){
+        //first click
+        hasFilppedCard = true;
+        firstCard = this;
+    }
+    else{
+        //second click
+        hasFilppedCard = false;
+        secondCard = this;
+
+        //do cards match?
+        console.log(firstCard.dataset.work);
+        console.log(secondCard.dataset.work);
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipcard));
